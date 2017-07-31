fcb42607f3124653c8222869acb1e03609cd5029
Kiyohiro Yamamoto <yamamoto_kiyohiro@cyberagent.co.jp>
2017-07-31 14:29:50 +0900

    Fix for using calc

--- hello.go
+++ hello.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/ref3000/codeship-test/calc"
+)
 
 func main() {
-	fmt.Printf("Hello World\n")
+	fmt.Printf("Hello World\n%d", calc.Sum(114, 514))
 }
